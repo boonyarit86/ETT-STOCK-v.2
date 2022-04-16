127510e35b5d3f936720eae680788c48da36554d
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-04-16 19:56:37 +0700

    Set initial style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import './App.scss';
+import './App.css';
+import Heading from './Components/Text/Heading';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <p>Hi</p>
+
+      <Test>
+        <Heading type="main" text="Heading 1" />
+        <Heading type="sub" text="Heading 2" />
+      </Test>
     </div>
   );
 }
